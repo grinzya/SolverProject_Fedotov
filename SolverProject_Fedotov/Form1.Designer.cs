51efc38fdf6c3ec854a74cb306a23f49c5fdddad
Федотов Григорий <grinzya@yandex.ru>
2019-12-22 19:11:36 +0500

    Добавлены TextBox'ы

--- SolverProject_Fedotov/Form1.Designer.cs
+++ SolverProject_Fedotov/Form1.Designer.cs
@@ -28,96 +28,115 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
-            this.textBox7 = new System.Windows.Forms.TextBox();
-            this.textBox8 = new System.Windows.Forms.TextBox();
-            this.textBox9 = new System.Windows.Forms.TextBox();
-            this.textBox10 = new System.Windows.Forms.TextBox();
-            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.Tb_l_ogn = new System.Windows.Forms.TextBox();
+            this.Tb_l_1 = new System.Windows.Forms.TextBox();
+            this.Tb_l_2 = new System.Windows.Forms.TextBox();
+            this.Tb_trab = new System.Windows.Forms.TextBox();
+            this.Tb_tnp = new System.Windows.Forms.TextBox();
+            this.Tb_tokr = new System.Windows.Forms.TextBox();
+            this.Tb_C1 = new System.Windows.Forms.TextBox();
+            this.Tb_C2 = new System.Windows.Forms.TextBox();
+            this.Tb_x0 = new System.Windows.Forms.TextBox();
+            this.Tb_arab = new System.Windows.Forms.TextBox();
+            this.Tb_anar = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.lbl_l_ogn = new System.Windows.Forms.Label();
+            this.Lbl_l1 = new System.Windows.Forms.Label();
+            this.Lbl_l_2 = new System.Windows.Forms.Label();
+            this.Lbl_trab = new System.Windows.Forms.Label();
+            this.Lbl_tnp = new System.Windows.Forms.Label();
+            this.Lbl_tokr = new System.Windows.Forms.Label();
+            this.Lbl_C1 = new System.Windows.Forms.Label();
+            this.Lbl_C2 = new System.Windows.Forms.Label();
+            this.Lbl_x0 = new System.Windows.Forms.Label();
+            this.lbl_arab = new System.Windows.Forms.Label();
+            this.lbl_anar = new System.Windows.Forms.Label();
+            this.Gb_Koef_Tepl = new System.Windows.Forms.GroupBox();
+            this.Gb_Temp = new System.Windows.Forms.GroupBox();
+            this.Gb_Cost = new System.Windows.Forms.GroupBox();
+            this.Gb_K_Tepl = new System.Windows.Forms.GroupBox();
+            this.Gb_Koef_Tepl.SuspendLayout();
+            this.Gb_Temp.SuspendLayout();
+            this.Gb_Cost.SuspendLayout();
+            this.Gb_K_Tepl.SuspendLayout();
             this.SuspendLayout();
             // 
-            // textBox1
+            // Tb_l_ogn
             // 
-            this.textBox1.Location = new System.Drawing.Point(179, 48);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 0;
+            this.Tb_l_ogn.Location = new System.Drawing.Point(222, 23);
+            this.Tb_l_ogn.Name = "Tb_l_ogn";
+            this.Tb_l_ogn.Size = new System.Drawing.Size(100, 20);
+            this.Tb_l_ogn.TabIndex = 0;
             // 
-            // textBox2
+            // Tb_l_1
             // 
-            this.textBox2.Location = new System.Drawing.Point(179, 74);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 0;
+            this.Tb_l_1.Location = new System.Drawing.Point(222, 49);
+            this.Tb_l_1.Name = "Tb_l_1";
+            this.Tb_l_1.Size = new System.Drawing.Size(100, 20);
+            this.Tb_l_1.TabIndex = 0;
             // 
-            // textBox3
+            // Tb_l_2
             // 
-            this.textBox3.Location = new System.Drawing.Point(179, 100);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 0;
+            this.Tb_l_2.Location = new System.Drawing.Point(222, 75);
+            this.Tb_l_2.Name = "Tb_l_2";
+            this.Tb_l_2.Size = new System.Drawing.Size(100, 20);
+            this.Tb_l_2.TabIndex = 0;
             // 
-            // textBox4
+            // Tb_trab
             // 
-            this.textBox4.Location = new System.Drawing.Point(179, 150);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 20);
-            this.textBox4.TabIndex = 0;
+            this.Tb_trab.Location = new System.Drawing.Point(204, 26);
+            this.Tb_trab.Name = "Tb_trab";
+            this.Tb_trab.Size = new System.Drawing.Size(100, 20);
+            this.Tb_trab.TabIndex = 0;
             // 
-            // textBox5
+            // Tb_tnp
             // 
-            this.textBox5.Location = new System.Drawing.Point(179, 176);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(100, 20);
-            this.textBox5.TabIndex = 0;
+            this.Tb_tnp.Location = new System.Drawing.Point(204, 52);
+            this.Tb_tnp.Name = "Tb_tnp";
+            this.Tb_tnp.Size = new System.Drawing.Size(100, 20);
+            this.Tb_tnp.TabIndex = 0;
             // 
-            // textBox6
+            // Tb_tokr
             // 
-            this.textBox6.Location = new System.Drawing.Point(179, 202);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(100, 20);
-            this.textBox6.TabIndex = 0;
+            this.Tb_tokr.Location = new System.Drawing.Point(204, 78);
+            this.Tb_tokr.Name = "Tb_tokr";
+            this.Tb_tokr.Size = new System.Drawing.Size(100, 20);
+            this.Tb_tokr.TabIndex = 0;
             // 
-            // textBox7
+            // Tb_C1
             // 
-            this.textBox7.Location = new System.Drawing.Point(179, 249);
-            this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(100, 20);
-            this.textBox7.TabIndex = 0;
+            this.Tb_C1.Location = new System.Drawing.Point(205, 28);
+            this.Tb_C1.Name = "Tb_C1";
+            this.Tb_C1.Size = new System.Drawing.Size(100, 20);
+            this.Tb_C1.TabIndex = 0;
             // 
-            // textBox8
+            // Tb_C2
             // 
-            this.textBox8.Location = new System.Drawing.Point(179, 275);
-            this.textBox8.Name = "textBox8";
-            this.textBox8.Size = new System.Drawing.Size(100, 20);
-            this.textBox8.TabIndex = 0;
+            this.Tb_C2.Location = new System.Drawing.Point(205, 54);
+            this.Tb_C2.Name = "Tb_C2";
+            this.Tb_C2.Size = new System.Drawing.Size(100, 20);
+            this.Tb_C2.TabIndex = 0;
             // 
-            // textBox9
+            // Tb_x0
             // 
-            this.textBox9.Location = new System.Drawing.Point(179, 301);
-            this.textBox9.Name = "textBox9";
-            this.textBox9.Size = new System.Drawing.Size(100, 20);
-            this.textBox9.TabIndex = 0;
+            this.Tb_x0.Location = new System.Drawing.Point(642, 265);
+            this.Tb_x0.Name = "Tb_x0";
+            this.Tb_x0.Size = new System.Drawing.Size(100, 20);
+            this.Tb_x0.TabIndex = 0;
             // 
-            // textBox10
+            // Tb_arab
             // 
-            this.textBox10.Location = new System.Drawing.Point(179, 346);
-            this.textBox10.Name = "textBox10";
-            this.textBox10.Size = new System.Drawing.Size(100, 20);
-            this.textBox10.TabIndex = 0;
+            this.Tb_arab.Location = new System.Drawing.Point(204, 28);
+            this.Tb_arab.Name = "Tb_arab";
+            this.Tb_arab.Size = new System.Drawing.Size(100, 20);
+            this.Tb_arab.TabIndex = 0;
             // 
-            // textBox11
+            // Tb_anar
             // 
-            this.textBox11.Location = new System.Drawing.Point(179, 372);
-            this.textBox11.Name = "textBox11";
-            this.textBox11.Size = new System.Drawing.Size(100, 20);
-            this.textBox11.TabIndex = 0;
+            this.Tb_anar.Location = new System.Drawing.Point(204, 54);
+            this.Tb_anar.Name = "Tb_anar";
+            this.Tb_anar.Size = new System.Drawing.Size(100, 20);
+            this.Tb_anar.TabIndex = 0;
             // 
             // button1
             // 
@@ -125,29 +144,187 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "Рассчитать";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbl_l_ogn
+            // 
+            this.lbl_l_ogn.AutoSize = true;
+            this.lbl_l_ogn.Location = new System.Drawing.Point(14, 26);
+            this.lbl_l_ogn.Name = "lbl_l_ogn";
+            this.lbl_l_ogn.Size = new System.Drawing.Size(195, 13);
+            this.lbl_l_ogn.TabIndex = 2;
+            this.lbl_l_ogn.Text = "материала огнеупорной стенки (lогн)";
+            // 
+            // Lbl_l1
+            // 
+            this.Lbl_l1.AutoSize = true;
+            this.Lbl_l1.Location = new System.Drawing.Point(14, 52);
+            this.Lbl_l1.Name = "Lbl_l1";
+            this.Lbl_l1.Size = new System.Drawing.Size(153, 13);
+            this.Lbl_l1.TabIndex = 2;
+            this.Lbl_l1.Text = "1-го материала изоляции (l1)";
+            // 
+            // Lbl_l_2
+            // 
+            this.Lbl_l_2.AutoSize = true;
+            this.Lbl_l_2.Location = new System.Drawing.Point(14, 78);
+            this.Lbl_l_2.Name = "Lbl_l_2";
+            this.Lbl_l_2.Size = new System.Drawing.Size(156, 13);
+            this.Lbl_l_2.TabIndex = 2;
+            this.Lbl_l_2.Text = " 2-го материала изоляции (l2)";
+            // 
+            // Lbl_trab
+            // 
+            this.Lbl_trab.AutoSize = true;
+            this.Lbl_trab.Location = new System.Drawing.Point(15, 26);
+            this.Lbl_trab.Name = "Lbl_trab";
+            this.Lbl_trab.Size = new System.Drawing.Size(132, 13);
+            this.Lbl_trab.TabIndex = 2;
+            this.Lbl_trab.Text = "рабочей среды в печи (t\')";
+            // 
+            // Lbl_tnp
+            // 
+            this.Lbl_tnp.AutoSize = true;
+            this.Lbl_tnp.Location = new System.Drawing.Point(15, 50);
+            this.Lbl_tnp.Name = "Lbl_tnp";
+            this.Lbl_tnp.Size = new System.Drawing.Size(124, 13);
+            this.Lbl_tnp.TabIndex = 2;
+            this.Lbl_tnp.Text = "окружающей среды (t\'\')";
+            // 
+            // Lbl_tokr
+            // 
+            this.Lbl_tokr.AutoSize = true;
+            this.Lbl_tokr.Location = new System.Drawing.Point(15, 78);
+            this.Lbl_tokr.Name = "Lbl_tokr";
+            this.Lbl_tokr.Size = new System.Drawing.Size(45, 13);
+            this.Lbl_tokr.TabIndex = 2;
+            this.Lbl_tokr.Text = "Lbl_tokr";
+            // 
+            // Lbl_C1
+            // 
+            this.Lbl_C1.AutoSize = true;
+            this.Lbl_C1.Location = new System.Drawing.Point(16, 28);
+            this.Lbl_C1.Name = "Lbl_C1";
+            this.Lbl_C1.Size = new System.Drawing.Size(107, 13);
+            this.Lbl_C1.TabIndex = 2;
+            this.Lbl_C1.Text = "1-го материала (С1)";
+            // 
+            // Lbl_C2
+            // 
+            this.Lbl_C2.AutoSize = true;
+            this.Lbl_C2.Location = new System.Drawing.Point(16, 54);
+            this.Lbl_C2.Name = "Lbl_C2";
+            this.Lbl_C2.Size = new System.Drawing.Size(107, 13);
+            this.Lbl_C2.TabIndex = 2;
+            this.Lbl_C2.Text = "1-го материала (С2)";
+            // 
+            // Lbl_x0
+            // 
+            this.Lbl_x0.AutoSize = true;
+            this.Lbl_x0.Location = new System.Drawing.Point(410, 265);
+            this.Lbl_x0.Name = "Lbl_x0";
+            this.Lbl_x0.Size = new System.Drawing.Size(207, 13);
+            this.Lbl_x0.TabIndex = 2;
+            this.Lbl_x0.Text = "Суммарная толщина изоляции, мм (X0)";
+            // 
+            // lbl_arab
+            // 
+            this.lbl_arab.AutoSize = true;
+            this.lbl_arab.Location = new System.Drawing.Point(15, 28);
+            this.lbl_arab.Name = "lbl_arab";
+            this.lbl_arab.Size = new System.Drawing.Size(68, 13);
+            this.lbl_arab.TabIndex = 2;
+            this.lbl_arab.Text = "внутренний;";
+            // 
+            // lbl_anar
+            // 
+            this.lbl_anar.AutoSize = true;
+            this.lbl_anar.Location = new System.Drawing.Point(15, 54);
+            this.lbl_anar.Name = "lbl_anar";
+            this.lbl_anar.Size = new System.Drawing.Size(61, 13);
+            this.lbl_anar.TabIndex = 2;
+            this.lbl_anar.Text = "наружный;";
+            // 
+            // Gb_Koef_Tepl
+            // 
+            this.Gb_Koef_Tepl.Controls.Add(this.Tb_l_2);
+            this.Gb_Koef_Tepl.Controls.Add(this.Tb_l_ogn);
+            this.Gb_Koef_Tepl.Controls.Add(this.Tb_l_1);
+            this.Gb_Koef_Tepl.Controls.Add(this.lbl_l_ogn);
+            this.Gb_Koef_Tepl.Controls.Add(this.Lbl_l1);
+            this.Gb_Koef_Tepl.Controls.Add(this.Lbl_l_2);
+            this.Gb_Koef_Tepl.Location = new System.Drawing.Point(31, 10);
+            this.Gb_Koef_Tepl.Name = "Gb_Koef_Tepl";
+            this.Gb_Koef_Tepl.Size = new System.Drawing.Size(328, 118);
+            this.Gb_Koef_Tepl.TabIndex = 3;
+            this.Gb_Koef_Tepl.TabStop = false;
+            this.Gb_Koef_Tepl.Text = "Коэффициенты теплопроводности";
+            // 
+            // Gb_Temp
+            // 
+            this.Gb_Temp.Controls.Add(this.Lbl_tokr);
+            this.Gb_Temp.Controls.Add(this.Tb_trab);
+            this.Gb_Temp.Controls.Add(this.Tb_tnp);
+            this.Gb_Temp.Controls.Add(this.Tb_tokr);
+            this.Gb_Temp.Controls.Add(this.Lbl_trab);
+            this.Gb_Temp.Controls.Add(this.Lbl_tnp);
+            this.Gb_Temp.Location = new System.Drawing.Point(426, 12);
+            this.Gb_Temp.Name = "Gb_Temp";
+            this.Gb_Temp.Size = new System.Drawing.Size(316, 115);
+            this.Gb_Temp.TabIndex = 4;
+            this.Gb_Temp.TabStop = false;
+            this.Gb_Temp.Text = "Температуры";
+            // 
+            // Gb_Cost
+            // 
+            this.Gb_Cost.Controls.Add(this.Tb_C1);
+            this.Gb_Cost.Controls.Add(this.Tb_C2);
+            this.Gb_Cost.Controls.Add(this.Lbl_C1);
+            this.Gb_Cost.Controls.Add(this.Lbl_C2);
+            this.Gb_Cost.Location = new System.Drawing.Point(50, 140);
+            this.Gb_Cost.Name = "Gb_Cost";
+            this.Gb_Cost.Size = new System.Drawing.Size(305, 84);
+            this.Gb_Cost.TabIndex = 5;
+            this.Gb_Cost.TabStop = false;
+            this.Gb_Cost.Text = "Стоимости материалов изоляции";
+            // 
+            // Gb_K_Tepl
+            // 
+            this.Gb_K_Tepl.Controls.Add(this.Tb_anar);
+            this.Gb_K_Tepl.Controls.Add(this.Tb_arab);
+            this.Gb_K_Tepl.Controls.Add(this.lbl_arab);
+            this.Gb_K_Tepl.Controls.Add(this.lbl_anar);
+            this.Gb_K_Tepl.Location = new System.Drawing.Point(39, 248);
+            this.Gb_K_Tepl.Name = "Gb_K_Tepl";
+            this.Gb_K_Tepl.Size = new System.Drawing.Size(316, 108);
+            this.Gb_K_Tepl.TabIndex = 6;
+            this.Gb_K_Tepl.TabStop = false;
+            this.Gb_K_Tepl.Text = "Коэффициенты теплоотдачи";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Gb_K_Tepl);
+            this.Controls.Add(this.Tb_x0);
+            this.Controls.Add(this.Gb_Cost);
+            this.Controls.Add(this.Gb_Temp);
+            this.Controls.Add(this.Gb_Koef_Tepl);
+            this.Controls.Add(this.Lbl_x0);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox11);
-            this.Controls.Add(this.textBox10);
-            this.Controls.Add(this.textBox9);
-            this.Controls.Add(this.textBox8);
-            this.Controls.Add(this.textBox7);
-            this.Controls.Add(this.textBox6);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Форма рассчёта";
+            this.Gb_Koef_Tepl.ResumeLayout(false);
+            this.Gb_Koef_Tepl.PerformLayout();
+            this.Gb_Temp.ResumeLayout(false);
+            this.Gb_Temp.PerformLayout();
+            this.Gb_Cost.ResumeLayout(false);
+            this.Gb_Cost.PerformLayout();
+            this.Gb_K_Tepl.ResumeLayout(false);
+            this.Gb_K_Tepl.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -155,18 +332,33 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.TextBox textBox6;
-        private System.Windows.Forms.TextBox textBox7;
-        private System.Windows.Forms.TextBox textBox8;
-        private System.Windows.Forms.TextBox textBox9;
-        private System.Windows.Forms.TextBox textBox10;
-        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.TextBox Tb_l_ogn;
+        private System.Windows.Forms.TextBox Tb_l_1;
+        private System.Windows.Forms.TextBox Tb_l_2;
+        private System.Windows.Forms.TextBox Tb_trab;
+        private System.Windows.Forms.TextBox Tb_tnp;
+        private System.Windows.Forms.TextBox Tb_tokr;
+        private System.Windows.Forms.TextBox Tb_C1;
+        private System.Windows.Forms.TextBox Tb_C2;
+        private System.Windows.Forms.TextBox Tb_x0;
+        private System.Windows.Forms.TextBox Tb_arab;
+        private System.Windows.Forms.TextBox Tb_anar;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lbl_l_ogn;
+        private System.Windows.Forms.Label Lbl_l1;
+        private System.Windows.Forms.Label Lbl_l_2;
+        private System.Windows.Forms.Label Lbl_trab;
+        private System.Windows.Forms.Label Lbl_tnp;
+        private System.Windows.Forms.Label Lbl_tokr;
+        private System.Windows.Forms.Label Lbl_C1;
+        private System.Windows.Forms.Label Lbl_C2;
+        private System.Windows.Forms.Label Lbl_x0;
+        private System.Windows.Forms.Label lbl_arab;
+        private System.Windows.Forms.Label lbl_anar;
+        private System.Windows.Forms.GroupBox Gb_Koef_Tepl;
+        private System.Windows.Forms.GroupBox Gb_Temp;
+        private System.Windows.Forms.GroupBox Gb_Cost;
+        private System.Windows.Forms.GroupBox Gb_K_Tepl;
     }
 }
 
